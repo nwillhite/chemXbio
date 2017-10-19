951390d09b48d89f65c2d6b2eb9951478781f072
nwillhite <nwill016@ucr.edu>
2017-10-19 00:43:53 -0700

    changed the way outputs work as inputs. allowed for consuming limited inputs based on operation outputs

--- js/operationInput.js
+++ js/operationInput.js
@@ -145,7 +145,6 @@ function operationInput(id) {
             container.removeChild(container.lastChild);
         }
 
-        var tmp = operationList.length;
         var sublength = substancelist.length;
 
         var option = document.createElement('option');
@@ -155,6 +154,7 @@ function operationInput(id) {
         option.setAttribute('style', 'display: none');
         container.appendChild(option);
 
+        /*
         // creates the input entries based on number selected with # of inputs
         // adds the output of operations to input drop down list
         for (i = 0; i < tmp; i++) {
@@ -195,8 +195,34 @@ function operationInput(id) {
                     }
                 }
             }
+        } */
+
+        /*
+        for (i = 0; i < sublength; i ++) {
+            var option = document.createElement('option');
+            option.innerHTML = substancelist[i].VARIABLE_DECLARATION.NAME;
+            option.value = substancelist[i].VARIABLE_DECLARATION.NAME;
+            option.className = 'substance';
+            container.appendChild(option);
         }
+
+        var option = document.createElement('option');
+        option.setAttribute('disabled', 'disabled');
+        option.innerHTML = option.value = '----';
+        container.appendChild(option);
+
+        for (i = 0; i < outputHolder.length; i++) {
+
+            if (outputHolder[i].status !== 'consumed') {
+                var option = document.createElement('option');
+                option.innerHTML = option.value = outputHolder[i].NAME;
+                container.appendChild(option);
+            }
+        }
+        */
+
     }
+
 
     for (i = 0; i < sublength; i ++) {
         var option = document.createElement('option');
@@ -204,6 +230,15 @@ function operationInput(id) {
         option.value = substancelist[i].VARIABLE_DECLARATION.NAME;
         option.className = 'substance';
         container.appendChild(option);
+    }
+
+    for (i = 0; i < outputHolder.length; i++) {
+
+        if (outputHolder[i].status !== 'consumed') {
+            var option = document.createElement('option');
+            option.innerHTML = option.value = outputHolder[i].NAME;
+            container.appendChild(option);
+        }
     }
 }
 
